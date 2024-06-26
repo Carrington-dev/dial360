73938d95861d5b4eb1be8860324f3106d976e4dd
Carrington-dev <crn96m@gmail.com>
2024-06-26 20:51:05 +0200

    Added JWT Endpoints

--- authweb/authweb/urls.py
+++ authweb/authweb/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+version = 'v1'
+
 urlpatterns = [
     path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
+    path(f'api/{version}/auth/', include('djoser.urls')),
+    path(f'api/{version}/auth/', include('djoser.urls.jwt')),
 ]
