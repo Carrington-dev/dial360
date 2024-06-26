e7649de74e1a1cbb43093af53161ea3353209386
Carrington-dev <crn96m@gmail.com>
2024-06-26 21:01:14 +0200

    Added Title Admin

--- authweb/authweb/urls.py
+++ authweb/authweb/urls.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from authweb import  settings
+from django.conf.urls.static import static
+
+admin.site.site_header = 'Dial360 '                   
+admin.site.index_title = 'Dial360 Portal'                 
+admin.site.site_title = 'Dial360 Administration'
 
 version = 'v1'
 
@@ -9,3 +15,8 @@ urlpatterns = [
     path(f'api/{version}/auth/', include('djoser.urls')),
     path(f'api/{version}/auth/', include('djoser.urls.jwt')),
 ]
+
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
