4cd43d8a05d7b43fec19b4a5e481f22d13a6d2d7
Carrington-dev <crn96m@gmail.com>
2024-06-26 20:40:51 +0200

    Started Auth User Model

--- authweb/authweb/settings.py
+++ authweb/authweb/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from decouple import config
 
@@ -19,7 +20,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    # 'rest_framework.authtoken', # new line for token
+    'rest_framework_simplejwt', #new line
+    'djoser',
     'security',
+
+   
+    
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -85,3 +94,83 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'security.User'
+
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://www.example.com",
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+    'http://localhost:*',
+    
+]
+
+AUTH_USER_MODEL = 'security.User'
+
+MEDIA_URL = 'media/'
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'extras/static/')
+MEDIA_ROOT = os.path.join(BASE_DIR,'extras/media/')
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'extra/static')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'extra/media')
+
+
+STATICFILES_DIRS = [
+    # BASE_DIR / "static",
+    os.path.join(BASE_DIR,'extras/assets/')
+    # "/var/www/static/",
+]
+
+
+DJOSER = {
+    'PASSWORD_RESET_CONFIRM_URL': 'secrets/password/reset/confirm/{uid}/{token}',
+    'USERNAME_RESET_CONFIRM_URL': 'secrets/username/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': 'secrets/activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL': True,
+    'PASSWORD_CHANGED_EMAIL_CONFIRMATION': True,
+    'USERNAME_CHANGED_EMAIL_CONFIRMATION': True,
+    'LOGOUT_ON_PASSWORD_CHANGE': True,
+    # 'USERNAME_RESET_SHOW_EMAIL_NOT_FOUND': True,
+    'SERIALIZERS': {
+        'user_create': 'security.serializers.UserCreateSerializer',
+        'user': 'security.serializers.UserCreateSerializer',
+        # 'user_create': 'djoser.serializers.UserCreateSerializer',
+    },
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+    
+   
+}
+
+
+GRAPPELLI_ADMIN_TITLE = "Oftmart Inc"
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_PORT = config("EMAIL_PORT")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = config("EMAIL_USE_TLS")
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
+DEFAULT_FROM_ADMIN = config("DEFAULT_FROM_ADMIN")
+
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('JWT',),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': True,
+}
--- authweb/security/models.py
+++ authweb/security/models.py
@@ -1,3 +1,20 @@
+from uuid import uuid4
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from security.leaders import UserManager
 
-# Create your models here.
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    username = models.CharField(max_length=200, unique=True)
+    email = models.EmailField(max_length=254, unique=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [ 'username' ]
+
+    def __str__(self) -> str:
+        return self.username
