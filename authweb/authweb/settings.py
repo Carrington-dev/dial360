842e807e301f5cab0b4e797875151d2114e5c597
Carrington {Muleya} <79579279+Carrington-dev@users.noreply.github.com>
2024-09-16 19:08:27 +0200

    Update settings.py

--- authweb/authweb/settings.py
+++ authweb/authweb/settings.py
@@ -5,7 +5,7 @@ from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default=True, cast=bool)
 
 DEBUG = config('DEBUG')
 
